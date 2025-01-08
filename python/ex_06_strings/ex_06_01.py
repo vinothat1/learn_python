aa9a92e868e8638715bff2ed76b9b8fd39ac0045
vinothat1@gmail.com <vinothmayilvaganan@Vinoths-MacBook-Air.local>
2025-01-08 21:00:43 +0100

    This is updated today

--- python/ex_06_strings/ex_06_01.py
+++ python/ex_06_strings/ex_06_01.py
@@ -8,13 +8,13 @@ index_0=inp[0]
 print("Index 0 letter is: ",index_0)
 index_1=inp[1]
 print("Index 1 letter is: ",index_1)
+#To print the nth string of the input
 index_n=inp[length-1]
 print("Index n letter is: ",index_n)
 #To print the n-1 th string of the input
 index_n_1=inp[length-2]
-#To print the nth string of the input
-print("Index n letter is: ",index_n_1)
+print("Index n-1 letter is: ",index_n_1)
 #To print the length of the input
-print("index(length-1): ",length-1)
+print("index(length-1) of the word starts from, 0 to",length-1)
 #To print the input by calling the strings
 print(inp[0:length])
